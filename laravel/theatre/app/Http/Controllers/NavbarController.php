27b72e9a007b1cd0a138d82154924cb4796ae7de
Artyyyom <gritsayartyomgritsay@gmail.com>
2018-02-09 21:43:33 +0300

    Add getNavbars api

--- laravel/theatre/app/Http/Controllers/NavbarController.php
+++ laravel/theatre/app/Http/Controllers/NavbarController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class NavbarController extends SiteController
 {
-    //
+    public function __construct() {
+    	parent::__construct(new \App\Repositories\NavbarsRepository(new \App\Navbar));
+    }
+
+    public function getNavbars() {
+    	print_r($this->n_rep->get());
+
+    }
 }
--- laravel/theatre/app/Http/Controllers/SiteController.php
+++ laravel/theatre/app/Http/Controllers/SiteController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Repositories\NavbarsRepository;
 class SiteController
 {
-    
+	protected $n_rep;
+
+    public function __construct(NavbarsRepository $n_rep) {
+    	$this->n_rep = $n_rep;
+    }
 }
