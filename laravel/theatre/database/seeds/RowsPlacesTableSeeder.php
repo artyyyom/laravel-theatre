a8ad62dbcf2b9accbad82b5283995cdf6170c999
Artyyyom <gritsayartyomgritsay@gmail.com>
2018-02-14 04:44:50 +0300

    Edit RowsPlacesTableSeeder: add status field

--- laravel/theatre/database/seeds/RowsPlacesTableSeeder.php
+++ laravel/theatre/database/seeds/RowsPlacesTableSeeder.php
@@ -15,14 +15,16 @@ class RowsPlacesTableSeeder extends Seeder
             'row_id' => 1,
             'place_id' => 1,
             'category_id' => 1,
-            'price' => '120'            
+            'price' => '120',
+            'status' => 0            
         ]
         );
         DB::table('rows_places')->insert([
             'row_id' => 2,
             'place_id' => 1,
             'category_id' => 2,
-            'price' => '60'   
+            'price' => '60',
+            'status' => 0   
         ]
         );
     }
